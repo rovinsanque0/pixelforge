6c796c3ed5dd3457fb7c80bce2aa3697ab474007
rovinsanque0 <rovinsanque0@gmail.com>
2025-11-25 21:07:10 -0600

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  namespace :admin do
+    root "dashboard#index"
+    resources :products
+    resources :categories
+  end
 
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  root "products#index" # 2.1 front page
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :products, only: %i[index show]
+
+  resource :cart, only: [:show] do
+    post :add_item
+    patch :update_item
+    delete :remove_item
+  end
+
+  resources :orders, only: %i[new create show]
 end
