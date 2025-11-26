6542830641ae2dbd7d6b8fd5a05038e52361f655
rovinsanque0 <rovinsanque0@gmail.com>
2025-11-26 17:39:25 -0600

    fixing product page and paginator

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,12 +1,14 @@
 class Admin::BaseController < ApplicationController
+  layout "admin"          # tells Rails to use layouts/admin.html.erb
+
   before_action :authenticate_user!
   before_action :require_admin!
-
-  layout "admin"
 
   private
 
   def require_admin!
-    redirect_to root_path, alert: "Not authorized" unless current_user&.admin?
+    unless current_user&.admin?
+      redirect_to root_path, alert: "You are not authorized to access the admin area."
+    end
   end
 end
