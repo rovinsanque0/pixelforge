d45fae548d9613fa060846a97168b5416c50acae
rovinsanque0 <rovinsanque0@gmail.com>
2025-11-25 21:11:55 -0600

    products (admin view)

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,45 @@
+class Admin::ProductsController < Admin::BaseController
+  before_action :set_product, only: %i[edit update destroy]
+
+  def index
+    @products = Product.order(:name)
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path, notice: "Product created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @product.update(product_params)
+      redirect_to admin_products_path, notice: "Product updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path, notice: "Product deleted."
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :stock, :category_id)
+  end
+end
